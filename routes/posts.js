90fac25f6f116847641a9baf2fe4815b4b0e16dd
Jenkos <you@example.com>
2024-11-03 17:44:46 -0800

    Enable filtering posts by title using regex in the posts route.
    Added a filter method to the /api/posts/filter route that allows users to retrieve posts based on a provided title query parameter.
    
    ### Usage:
    Browse to: http://localhost:3000/api/posts/filter?title=<parameter>
    (e.g., 'divine' or 'remember')

--- routes/posts.js
+++ routes/posts.js
@@ -4,4 +4,99 @@ const router = express.Router();
 const posts = require("../data/posts");
 const error = require("../utilities/error");
 
+router
+ .route("/")
+ .get((req, res) => {
+    res.json(posts);
 
+ });
+
+
+
+router
+ .route("/:id")
+ .get((req, res, next) => {
+    const post = posts.find((p) => p.id == req.params.id);
+   if (post) res.json(post);
+   else next();
+ })
+
+ .post((req, res, next) => {
+    const {title, body, tags, reactions, views ,userId} = req.body;
+    if (!title) {
+        return res.status(400).json({ error: "Title is required." });
+    }
+       
+       //create new post
+        const newPost ={
+            id: posts.length + 1,
+            title,
+            body: body || "",
+            tags: tags || [],
+            reactions: reactions || 0,
+            views: views || 0, //default to 0 if not provides
+            userId: userId || null //default to null if not provided
+            
+        };
+
+
+        posts.push(newPost);
+        res.status(201).json(newPost);
+         
+ })
+ 
+ .patch((req, res, next) => {
+    const post = posts.find((p, i) => {
+        if (p.id == req.params.id){
+            for (const key in req.body){
+                posts[i][key] = req.body[key];
+            }
+            return true;
+        }
+    });
+    if(post) res.json(post);
+    else next();
+ })
+
+ .delete((req, res, next) => {
+    const post = posts.find((p, i) => {
+      if (p.id == req.params.id) {
+        posts.splice(i, 1);
+        return true;
+      }
+    });
+
+    if (post) res.json(post);
+    else next();
+  });
+
+   // filter posts by title 
+router
+ .route("/posts/filter")
+ .get((req, res) => {
+
+    const {title} = req.query;
+
+    if (!title){
+        return res.status(400).json({message: "Title query parameter is required"})
+    }
+
+    const regex = new RegExp(title, 'i');
+    const filteredPosts = posts.filter(post => regex.test(post.title));
+    
+    if (filteredPosts.length > 0)
+    res.json(filteredPosts);
+    else next();
+
+
+    
+  });
+
+
+
+
+
+
+
+//export the router
+module.exports = router;
